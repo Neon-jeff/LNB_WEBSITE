604bef8dbd90c34edd8f5672ed36cb0b965d68d3
Neon-jeff <omaksteejay@gmail.com>
2023-01-02 11:17:23 +0100

    Fixed mobile nav

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ let DocPage = document.querySelector("html");
 let mobileNav = document.querySelector(".mobile-nav");
 let closeMobileNav = document.querySelector(".mobile-nav-view img");
 let openMobileNav = document.querySelector(".mobile-nav img");
-let mobileNavView = document.querySelector(".mobile-nav-view");
+let mobileNavView = document.querySelector(".mobile-nav-view ");
 let tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 let menuBtn = document.querySelectorAll(".img-btn button");
 let menuList = document.querySelectorAll(".img-container");
+let mobileNavLinks = document.querySelectorAll(".mobile-nav-view p");
 
 gsap.registerPlugin(ScrollTrigger);
-
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    gsap.fromTo(
+      ".mobile-nav-view",
+      { x: "0", duration: 1, opacity: 1 },
+      { x: "-100%", opacity: 0 }
+    );
+  });
+});
 window.addEventListener("scroll", () => {
   if (window.scrollY > headerSection.scrollHeight - 300) {
     deskNav.className = "nav-class";
   } else {
     deskNav.classList.remove("nav-class");
   }
-
 });
 function MobileView() {
   if (headerSection.clientWidth <= 481) {
@@ -34,26 +42,23 @@ openMobileNav.addEventListener("click", () => {
   gsap.fromTo(
     ".mobile-nav-view",
     { x: "-100%", opacity: 0 },
-    { x: "0", duration: 1, opacity: 1 },
-    
+    { x: "0", duration: 1, opacity: 1 }
   );
 });
 closeMobileNav.addEventListener("click", () => {
-    gsap.fromTo(
-      ".mobile-nav-view",
-      { x: "0", duration: 1, opacity: 1 },
-      { x: "-100%", opacity: 0 }
-    );
+  gsap.fromTo(
+    ".mobile-nav-view",
+    { x: "0", duration: 1, opacity: 1 },
+    { x: "-100%", opacity: 0 }
+  );
   // mobileNavView.classList.add("hide");
-
 });
 //Button Menu Initial States
-for(i=0;i<menuBtn.length;i++){
-  if(i==0){
-    menuBtn[i].id='btn-active'
-  }
-  else{
-    menuBtn[i].removeAttribute('id')
+for (i = 0; i < menuBtn.length; i++) {
+  if (i == 0) {
+    menuBtn[i].id = "btn-active";
+  } else {
+    menuBtn[i].removeAttribute("id");
   }
 }
 for (i = 0; i < menuList.length; i++) {
@@ -75,9 +80,9 @@ menuBtn.forEach((btn) => {
             menuList[j].classList[1].slice(0, 3) !==
             btn.textContent.toLowerCase().slice(0, 3)
           ) {
-            menuList[j].id='hide'
+            menuList[j].id = "hide";
           } else {
-            menuList[j].removeAttribute('id')
+            menuList[j].removeAttribute("id");
           }
         }
       } else {
@@ -119,15 +124,19 @@ gsap.from(".welcome-txt p", {
   duration: 2,
   stagger: 0.5,
 });
-gsap.from(".img-container", {
-  scrollTrigger: {
-    trigger: ".menu ",
-    toggleActions: "restart restart restart restart",
+gsap.from(
+  ".img-container",
+  {
+    scrollTrigger: {
+      trigger: ".menu ",
+      toggleActions: "restart restart restart restart",
+    },
+    x: "-100%",
+    duration: 1,
+    stagger: 0.5,
   },
-  x: "-100%",
-  duration: 1,
-  stagger: 0.5,
-},'-=2');
+  "-=2"
+);
 gsap.from(
   ".choose h2",
   {
@@ -182,4 +191,3 @@ gsap.from(
   },
   "-=2"
 );
-
